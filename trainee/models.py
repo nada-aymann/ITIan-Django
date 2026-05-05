5ac323519e84a0d020e335476a229996babf56c0
nada-aymann <na835126@gmail.com>
2026-05-05 20:54:08 +0300

    add trainee model

--- trainee/models.py
+++ trainee/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Trainee(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    age = models.IntegerField()
+    
